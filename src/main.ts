4ee839a23c436b2206e10602af236d47e34ccd5b
FedeGonzalez <198593483+federicogonzalez97@users.noreply.github.com>
2025-08-22 11:13:19 -0300

    ADD: add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Properties BE API')
+    .setDescription('The Properties BE API for the Properties FE')
+    .setVersion('1.0')
+    .addTag('Properties')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
 }
-bootstrap();
+void bootstrap();
